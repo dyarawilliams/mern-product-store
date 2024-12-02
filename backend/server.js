f9ae1783e8c4b6fc7a3db42234a05c5c853d5feb
D'yara Williams <dyara.williams@gmail.com>
2024-12-01 22:30:38 -0500

    change PORT into env variable

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from "./routes/product.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000
 
 app.use(express.json()); // allows us to accept json data in the req.body
 
 app.use("/api/products",  productRoutes)
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:8000");
+    console.log(`Server started at http://localhost:${PORT}`);
 });
