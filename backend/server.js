2339fa11e08877fb1c3ee274bfc14436eba94a64
D'yara Williams <dyara.williams@gmail.com>
2024-12-01 22:26:07 -0500

    completed backend with routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js"
 
+import productRoutes from "./routes/product.js";
+
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    
-});
+app.use(express.json()); // allows us to accept json data in the req.body
+
+app.use("/api/products",  productRoutes)
 
 app.listen(8000, () => {
     connectDB();
