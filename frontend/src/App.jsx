8f0eb435896cb87bb9d567d864a0ade0d3885d60
D'yara Williams <dyara.williams@gmail.com>
2024-12-03 14:40:19 -0500

    navbar completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { Box } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar.jsx"
+import { useColorModeValue } from "./components/ui/color-mode.jsx";
 import CreatePage from "./pages/CreatePage.jsx"
 import HomePage from "./pages/HomePage.jsx"
 
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH="100vh" bg={useColorModeValue("purple.300", "black")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,42 +1,46 @@
-import { Button, Container, Flex, Text, HStack } from "@chakra-ui/react";
+import { Container, Flex, Text, HStack } from "@chakra-ui/react";
+import { Button } from "../components/ui/button.jsx";
+import { useColorMode } from "../components/ui/color-mode.jsx";
 import { Link } from "react-router-dom";
+
 import { BsPlusCircle } from "react-icons/bs";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 
 const Navbar = () => {
-  return (
-    <Container maxW="1140px" px={4}>
-        <Flex 
-            h={16} 
-            align="center" 
-            justify="space-between"
-            flexDir={{
-                base: "column",
-                sm: "row"
-            }}
-        >
-            <Text
-                fontSize={{ base: "22", sm: "28" }}
-                fontWeight="bold"
-                textTransform="uppercase"
-                textAlign="center"
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+        <Container maxWidth="1140px" px="4">
+            <Flex 
+                height="16" 
+                align="center" 
+                justify="space-between"
+                direction={{ base: "column", sm: "row" }}
             >
-                <Link to={"/"}>Product Store</Link>
-            </Text>
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                    textAlign="center"
+                >
+                    <Link to={"/"}>Product Store</Link>
+                </Text>
 
-            <HStack spacing={2} alignItems={"center"}>
-				<Link to={"/create"}>
-					<Button>
-						<BsPlusCircle fontSize={20} />
-					</Button>
-				</Link>
-				{/* <Button onClick={toggleColorMode}>
-					{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
-				</Button> */}
-				</HStack>
-        </Flex>
-    </Container>
-  )
-}
+                <HStack spacing="2" align="center">
+                    <Link to={"/create"}>
+                        <Button>
+                            <BsPlusCircle fontSize="20" />
+                        </Button>
+                    </Link>
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+                    </Button>
+                </HStack>
+            </Flex>
+        </Container>
+    );
+};
 
-export default Navbar
+export default Navbar;
