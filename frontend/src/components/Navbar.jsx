a8f3ee48ba6cef5c0deb6a179c26130cc71d444a
D'yara Williams <dyara.williams@gmail.com>
2024-12-02 23:06:36 -0500

    initial setup for frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Button, Container, Flex, Text, HStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { BsPlusCircle } from "react-icons/bs";
+
+
+const Navbar = () => {
+  return (
+    <Container maxW="1140px" px={4}>
+        <Flex 
+            h={16} 
+            align="center" 
+            justify="space-between"
+            flexDir={{
+                base: "column",
+                sm: "row"
+            }}
+        >
+            <Text
+                color="white"
+                fontSize={{ base: "22", sm: "28" }}
+                fontWeight="bold"
+                textTransform="uppercase"
+                textAlign="center"
+                bgClip="text"
+            >
+                <Link to={"/"}>Product Store</Link>
+            </Text>
+
+            <HStack spacing={2} alignItems={"center"}>
+				<Link to={"/create"}>
+					<Button>
+						<BsPlusCircle fontSize={20} />
+					</Button>
+				</Link>
+				{/* <Button onClick={toggleColorMode}>
+					{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+				</Button> */}
+				</HStack>
+        </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
